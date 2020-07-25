4aa480bd00eb39ca8e0eaafc98e236295255cfb9
Harpreet Tohra <harpreet.s@chitkara.edu.in>
2020-07-25 14:26:49 +0530

    commit

--- web/index.php
+++ web/index.php
@@ -5,15 +5,9 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+
+<?php include_once(“home.html”); ?>
 
 // Our web handlers
 
